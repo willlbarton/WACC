719590efdd71d1e05ad2b26a90f1349b28ad73a4
Ethan Hosier <ethanjhosier@gmail.com>
2024-01-23 13:40:44 +0000

    [eh1322] refactor(syntax.scala): added comments

--- src/main/wacc/syntax.scala
+++ src/main/wacc/syntax.scala
@@ -10,18 +10,20 @@ sealed trait Type
 sealed trait PairElemType extends Type
 sealed trait BaseType extends PairElemType
 
-//Base types
+// <base-type>
 case object IntType extends BaseType
 case object BoolType extends BaseType
 case object CharType extends BaseType
 case object StringType extends BaseType
 
-//Pair elem types
+// <pair-elem-type)
 case class ArrayType(t: Type) extends PairElemType
 case object Pair extends PairElemType
 
+// <pair-type>
 case class PairType(fst: PairElemType, snd: PairElemType) extends Type
 
+// <stmnt>
 sealed trait Stmt
 case object Skip extends Stmt
 case class Decl(t: Type, name: Ident, value: RVal) extends Stmt
@@ -37,22 +39,46 @@ case class While(cond: Expr, body: Stmt) extends Stmt
 case class ScopedStmt(stmt: Stmt) extends Stmt
 case class StmtChain(stmt: Stmt, next: Stmt) extends Stmt
 
+// <rvalue>
 sealed trait RVal
 case class ArrayLiter(first: Expr, rest: List[Expr]) extends RVal
+
+// <lvalue>
 sealed trait LVal
 sealed trait Expr extends RVal
 case class UnaryApp(op: UnaryOp, expr: Expr) extends Expr
 case class BinaryApp(op: BinaryOp, left: Expr, right: Expr) extends Expr
-case class Integer(i: BigInt) extends Expr
+
+// <atom>
+case class Integer(i: Int) extends Expr
 case class Boolean(value: Boolean) extends Expr
 case class Character(c: Char) extends Expr
 case class StringAtom(s: String) extends Expr
-case object Null extends Expr // Pair literal
+case object Null extends Expr // <pair-liter>
 case class Ident(name: String) extends Expr
 case class ArrayElem(name: Ident, exprs: List[Expr]) extends Expr
 case class BracketedExpr(expr: Expr) extends Expr
 
+// <unary-oper>
 trait UnaryOp
+case object Exclamation extends UnaryOp
+case object Negation extends UnaryOp
+case object Len extends UnaryOp
+case object Ord extends UnaryOp
+case object Chr extends UnaryOp
 
+// <binary-oper>
 trait BinaryOp
-// ...
+case object Mul extends BinaryOp
+case object Div extends BinaryOp
+case object Mod extends BinaryOp
+case object Add extends BinaryOp
+case object Sub extends BinaryOp
+case object Gt extends BinaryOp
+case object GtEq extends BinaryOp
+case object Lt extends BinaryOp
+case object LtEq extends BinaryOp
+case object Eq extends BinaryOp
+case object NotEq extends BinaryOp
+case object And extends BinaryOp
+case object Or extends BinaryOp
