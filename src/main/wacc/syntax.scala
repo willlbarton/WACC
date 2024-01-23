8955a566ce6ed1df3cf94ee844f26a12df61e5e9
Francois Poh <francois.poh22@imperial.ac.uk>
2024-01-23 13:22:07 +0000

    [fp522] feat(syntax.scala): Add Stmt andd Expr case classes

--- src/main/wacc/syntax.scala
+++ src/main/wacc/syntax.scala
@@ -1,8 +1,10 @@
 package src.main.wacc
 
 case class Program(functions: List[Func], body: Stmt)
-case class Func(t: Type, name: String, parameters: List[Param], body: Stmt)
-case class Param(t: Type, name: String)
+
+// An empty 'params' list should be the same as no param-list in the syntax
+case class Func(t: Type, name: Ident, params: List[Param], body: Stmt)
+case class Param(t: Type, name: Ident)
 
 sealed trait Type
 sealed trait PairElemType extends Type
@@ -19,7 +21,36 @@ case object Pair extends PairElemType
 case class PairType(fst: PairElemType, snd: PairElemType) extends Type
 
 sealed trait Stmt
-// ...
+case object Skip extends Stmt
+case class Decl(t: Type, name: Ident, value: RVal) extends Stmt
+case class Asgn(name: Ident, value: RVal) extends Stmt
+case class Read(value: LVal) extends  Stmt
+case class Free(expr: Expr) extends Stmt
+case class Return(expr: Expr) extends Stmt
+case class Exit(expr: Expr) extends Stmt
+case class Print(expr: Expr) extends Stmt
+case class PrintLn(expr: Expr) extends Stmt
+case class IfStmt(cond: Expr, body1: Stmt, body2: Stmt) extends Stmt
+case class While(cond: Expr, body: Stmt) extends Stmt
+case class ScopedStmt(stmt: Stmt) extends Stmt
+case class StmtChain(stmt: Stmt, next: Stmt) extends Stmt
 
-sealed trait Expr
+sealed trait RVal
+case class ArrayLiter(first: Expr, rest: List[Expr]) extends RVal
+sealed trait LVal
+sealed trait Expr extends RVal
+case class UnaryApp(op: UnaryOp, expr: Expr) extends Expr
+case class BinaryApp(op: BinaryOp, left: Expr, right: Expr) extends Expr
+case class Integer(i: BigInt) extends Expr
+case class Boolean(value: Boolean) extends Expr
+case class Character(c: Char) extends Expr
+case class StringAtom(s: String) extends Expr
+case object Null extends Expr // Pair literal
+case class Ident(name: String) extends Expr
+case class ArrayElem(name: Ident, exprs: List[Expr]) extends Expr
+case class BracketedExpr(expr: Expr) extends Expr
+
+trait UnaryOp
+
+trait BinaryOp
 // ...
