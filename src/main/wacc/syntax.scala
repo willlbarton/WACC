5a1bff47b55db21121daf885302be37ae0046de5
Ethan Hosier <ethanjhosier@gmail.com>
2024-01-23 13:18:09 +0000

    [eh1322] feat(syntax.scala, .gitignore): add Type trait

--- src/main/wacc/syntax.scala
+++ src/main/wacc/syntax.scala
@@ -5,7 +5,18 @@ case class Func(t: Type, name: String, parameters: List[Param], body: Stmt)
 case class Param(t: Type, name: String)
 
 sealed trait Type
-// ...
+sealed trait PairElemType extends Type
+sealed trait BaseType extends PairElemType
+
+case object IntType extends BaseType
+case object BoolType extends BaseType
+case object CharType extends BaseType
+case object StringType extends BaseType
+
+case class ArrayType(t: Type) extends PairElemType
+case object Pair extends PairElemType
+
+case class PairType(fst: PairElemType, snd: PairElemType) extends Type
 
 sealed trait Stmt
 // ...
