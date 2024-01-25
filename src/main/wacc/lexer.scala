5b4ff718ddffd665640629003ff1b9575a982a69
Francois Poh <francois.poh22@imperial.ac.uk>
2024-01-25 14:39:54 +0000

    [fp522] feat(lexer.scala): Specify character and string parsers, change integer to nat

--- src/main/wacc/lexer.scala
+++ src/main/wacc/lexer.scala
@@ -3,6 +3,8 @@ package src.main.wacc
 import parsley.Parsley
 import parsley.token.{Lexer, predicate}
 import parsley.token.descriptions._
+import parsley.token.descriptions.text.{EscapeDesc, TextDesc}
+import parsley.token.predicate.Unicode
 import parsley.token.symbol.ImplicitSymbol
 
 object lexer {
@@ -14,11 +16,29 @@ object lexer {
       spaceDesc = SpaceDesc.plain.copy(
         lineCommentStart = "#",
       ),
+      textDesc = TextDesc.plain.copy(
+        escapeSequences = EscapeDesc.plain.copy(
+          literals = Set('\\', "\'", "\""),
+          mapping = Map(
+            '0' -> '\0',
+            'b' -> '\b',
+            't' -> '\t',
+            'n' -> '\n',
+            'f' -> '\f',
+            'r' -> '\r'
+          ),
+        ),
+        graphicCharacter = Unicode(c =>
+          c >= ' '.toInt && c != '\\'.toInt && c != '\"'.toInt && c != '\''.toInt
+        ),
+      ),
     )
     private val lexer = new Lexer(desc)
 
-    val ident: Parsley[Ident] = lexer.lexeme.names.identifier.map(Ident)
-    val integer: Parsley[BigInt] = lexer.lexeme.integer.decimal
+    val ident: Parsley[Ident] = Ident(lexer.lexeme.names.identifier)
+    val nat: Parsley[Int] = lexer.lexeme.natural.decimal.map(_.toInt)
+    val character: Parsley[Char] = lexer.lexeme.character.latin1
+    val string: Parsley[String] = lexer.lexeme.string.latin1
     val implicits: ImplicitSymbol = lexer.lexeme.symbol.implicits
     def fully[A](p: Parsley[A]): Parsley[A] = lexer.fully(p)
 }
