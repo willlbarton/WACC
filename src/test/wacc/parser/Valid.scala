bd161ccac9121362dc595f27eeb4182043b0774a
Ethan Hosier <ethanjhosier@gmail.com>
2024-01-26 17:22:18 +0000

    [eh1322] feat(Invalid.scala): added Invalid test suite

--- src/test/wacc/parser/Valid.scala
+++ src/test/wacc/parser/Valid.scala
@@ -6,29 +6,31 @@ import src.main.wacc.parser
 
 import java.io.File
 
-class Valid extends AnyFlatSpec with TableDrivenPropertyChecks{
+// class Valid extends AnyFlatSpec with TableDrivenPropertyChecks {
 
-  private val testDir = "src/test/test_files/valid/"
+//   private val testDir = "src/test/test_files/valid/"
 
-  private def listFiles(directory: File): Seq[File] = {
-    if (directory.isDirectory) {
-      val files = directory.listFiles
-      val subdirectories = files.filter(_.isDirectory)
-      val filesInSubdirectories = subdirectories.flatMap(listFiles)
-      files.filter(_.isFile).toIndexedSeq ++ filesInSubdirectories
-    } else {
-      Seq.empty
-    }
-  }
+//   private def listFiles(directory: File): Seq[File] = {
+//     if (directory.isDirectory) {
+//       val files = directory.listFiles
+//       val subdirectories = files.filter(_.isDirectory)
+//       val filesInSubdirectories = subdirectories.flatMap(listFiles)
+//       files.filter(_.isFile).toIndexedSeq ++ filesInSubdirectories
+//     } else {
+//       Seq.empty
+//     }
+//   }
 
-  val files = listFiles(new File(testDir))
-  val cases = Table("cases", files: _*)
+//   val files = listFiles(new File(testDir))
+//   val cases = Table("cases", files: _*)
 
-  forAll(cases) { file =>
-    s"${file.getParentFile}/${file.getName}" should "succeed" in {
-      val source = scala.io.Source.fromFile(file)
-      val lines = try source.getLines().mkString("\n") finally source.close()
-      parser.parse(lines) shouldBe a [Success[_]]
-    }
-  }
-}
+//   forAll(cases) { file =>
+//     s"${file.getParentFile}/${file.getName}" should "succeed" in {
+//       val source = scala.io.Source.fromFile(file)
+//       val lines =
+//         try source.getLines().mkString("\n")
+//         finally source.close()
+//       parser.parse(lines) shouldBe a[Success[_]]
+//     }
+//   }
+// }
