f16d725e74d889b005fc38ff3027422d5563c8d8
Francois Poh <francois.poh22@imperial.ac.uk>
2024-02-01 11:56:02 +0000

    [fp522] refactor(): Rename Var to Ident

--- src/main/wacc/SymbolTable.scala
+++ src/main/wacc/SymbolTable.scala
@@ -3,16 +3,16 @@ package src.main.wacc
 import scala.collection.mutable
 
 case class SymbolTable(parent: Option[SymbolTable]) {
-  private val table: mutable.Map[Var, Identifier] = mutable.HashMap()
+  private val table: mutable.Map[Ident, Identifier] = mutable.HashMap()
 
-  def update(ident: Var, obj: Identifier): Unit = table += ident -> obj
+  def update(ident: Ident, obj: Identifier): Unit = table += ident -> obj
   
   // Checks for the identifier in the current scope and all parent scopes
-  def apply(ident: Var): Option[Identifier] =
+  def apply(ident: Ident): Option[Identifier] =
     table.get(ident) orElse parent.flatMap(_.apply(ident))
   
   // Only checks within the current scope; apply may return a value even if contains returns false
-  def contains(ident: Var): Boolean = table.contains(ident)
+  def contains(ident: Ident): Boolean = table.contains(ident)
 
   val makeChild: SymbolTable = SymbolTable(Some(this))
 }
