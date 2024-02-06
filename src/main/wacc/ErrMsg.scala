316467dad0206e49ed3f04fbd6c036e315147f1c
Francois Poh <francois.poh22@imperial.ac.uk>
2024-02-06 11:52:22 +0000

    [fp522] refactor(ErrMsg.scala): Comments

--- src/main/wacc/ErrMsg.scala
+++ src/main/wacc/ErrMsg.scala
@@ -1,14 +1,18 @@
 package src.main.wacc
 
+// Error message typeclass
 trait ErrMsg {
+  // Adds context to an error message if it is not empty
   def withContext[A](ctx: A): String
 }
 
 object ErrMsg {
+  // Allows the use of the withContext method on any string
   implicit class stringErrMsg(err: String) {
     def withContext[A](ctx: A): String = if (err.nonEmpty) s"$err  in $ctx\n" else ""
   }
 
+  // Error message with mismatching type, requires a context
   def typeErrorMsg(situation: String, context: String, expected: String, got: String): String =
     s"Type mismatch error in $situation\n" +
       s"  Expected '$expected', but got '$got'\n" withContext context
