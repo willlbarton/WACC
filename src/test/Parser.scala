42c210d25cebfa3c634c2a8f44fbcfb9ab72f3cb
Francois Poh <francois.poh22@imperial.ac.uk>
2024-02-09 17:02:22 +0000

    [fp522] feat(Parser.scala): Structure tests

--- src/test/Parser.scala
+++ src/test/Parser.scala
@@ -4,7 +4,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers._
 import org.scalatest.prop.TableDrivenPropertyChecks
 import parsley.{Failure, Success}
-import src.main.wacc.parser
+import src.main.wacc._
 
 class Parser extends AnyFlatSpec with TableDrivenPropertyChecks {
 
@@ -22,4 +22,28 @@ class Parser extends AnyFlatSpec with TableDrivenPropertyChecks {
     }
   }
 
+  "parser-associativity" should "bind * and / tighter then +" in {
+    parser.parse(
+      """begin
+        |  int x = 1 * 1 + 1 / 2
+        |end
+        |""".stripMargin).get shouldBe
+      Program(List.empty,
+        Decl(IntType, Ident("x"), Add(Mul(Integer(1), Integer(1)), Div(Integer(1), Integer(2)))))
+  }
+  it should "not associate == and !=" in {
+    parser.parse(
+      """begin
+        |  bool x = 1 == 1 != 2
+        |end
+        |""".stripMargin) shouldBe a [Failure[_]]
+  }
+  it should "build statements from left to right" in {
+    parser.parse(
+      """begin
+        |  skip; skip; skip
+        |end
+        |""".stripMargin).get shouldBe
+      Program(List.empty, StmtChain(Skip, StmtChain(Skip, Skip)))
+  }
 }
