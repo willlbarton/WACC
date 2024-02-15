b0f154efba8e818e664cc8d079dd3a44184e2c68
Francois Poh <fp522@ic.ac.uk>
2024-02-15 14:54:03 +0000

    [fp522] feat(generator.scala): genProgram, update ControlFlowGraph

--- src/main/wacc/generator.scala
+++ src/main/wacc/generator.scala
@@ -5,5 +5,41 @@ object generator {
   def generate(program: Program, formatter: Formatter): String = genProgram(program).toList
     .map(formatter(_)).mkString("\n")
 
-  private def genProgram(program: Program): ControlFlowGraph = ???
+  private def genProgram(program: Program): ControlFlowGraph = {
+    val graph = ControlFlowGraph()
+    program.functions.foreach(x => graph.add(genFunc(x)))
+    graph.add(saveRegs(List.empty))
+    program.body.foreach(x => graph.add(genStmt(x)))
+    graph.add(restoreStack(List.empty))
+    graph
+  }
+
+  // save the stack pointer to enter a new scope
+  private def saveRegs(regs: List[Register]): ControlFlowGraph = {
+    val graph = ControlFlowGraph()
+    graph.add(CfgNode(Push(Register(Rbp)))) // Save stack pointer
+    graph.add(regs.map(r => CfgNode(Push(r))))
+    graph.add(CfgNode(Mov(Register(Rbp), Register(Rsp)))) // Set stack pointer to base pointer
+    graph
+  }
+
+  // restore the stack pointer to exit a scope
+  private def restoreStack(regs: List[Register]): ControlFlowGraph = {
+    val graph = ControlFlowGraph()
+    graph.add(regs.reverseIterator.map(r => CfgNode(Pop(r))).toList)
+    graph.add(CfgNode(Pop(Register(Rbp))))
+    graph
+  }
+
+  private def genFunc(func: Func): ControlFlowGraph = ???
+
+  private def genStmt(stmt: Stmt): ControlFlowGraph =
+    stmt match {
+      case Skip => ControlFlowGraph()
+      case Exit(expr) => genExit(expr)
+    }
+
+  private def genExit(expr: Expr): ControlFlowGraph = ???
+
+  private def genExpr(expr: Expr): ControlFlowGraph = ???
 }
