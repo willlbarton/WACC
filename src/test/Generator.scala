160e08a970e8410b47e9ef7596791147caad5656
Elvis Chan <wcc20@beech13.doc.ic.ac.uk>
2024-02-22 11:41:37 +0000

    [wcc20] fix(Generator.scala): behavior for valid and invalid semantic and syntax errors

--- src/test/Generator.scala
+++ src/test/Generator.scala
@@ -3,21 +3,28 @@ package src.test
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers._
 import org.scalatest.prop.TableDrivenPropertyChecks
-import src.main.wacc.{generator}
+import src.main.wacc.{generator, analyser, parser, x86Formatter}
 
 class Generator extends AnyFlatSpec with TableDrivenPropertyChecks {
 
   behavior of "generator-valid"
   forAll(Table("cases", TestFiles("valid/"): _*)) { file =>
     it should s"not produce errors for ${file.getParentFile}/${file.getName}" in {
-      generator.generate(analyser.analyse(parser.parse(TestFiles.getLines(file)).get)) should not be Exit 
+      generator.generate((parser.parse(TestFiles.getLines(file)).get), x86Formatter) should not contain ("Exit")
     }
   }
 
-  behavior of "generator-invalid"
-  forAll(Table("cases", TestFiles("invalid/"): _*)) { file =>
+  behavior of "generator-invalid-semanticErr"
+  forAll(Table("cases", TestFiles("invalid/semanticErr"): _*)) { file =>
     it should s"produce errors for ${file.getParentFile}/${file.getName}" in {
-      generator.generate(analyser.analyse(parser.parse(TestFiles.getLines(file)).get)) shouldBe Exit
+      generator.generate((parser.parse(TestFiles.getLines(file)).get), x86Formatter) should contain ("Exit: 200")
+    }
+  }
+
+  behavior of "generator-invalid-syntaxErr"
+  forAll(Table("cases", TestFiles("invalid/syntaxErr"): _*)) { file =>
+    it should s"produce errors for ${file.getParentFile}/${file.getName}" in {
+      generator.generate((parser.parse(TestFiles.getLines(file)).get), x86Formatter) should contain ("Exit: 100")
     }
   }
 
