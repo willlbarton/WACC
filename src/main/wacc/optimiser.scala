2ce8342b9a1171cdb2ed1b10976801458846b22f
Francois Poh <francois.poh22@imperial.ac.uk>
2024-02-29 18:33:41 +0000

    [fp522] feat(optimiser): remove push-pop

--- src/main/wacc/optimiser.scala
+++ src/main/wacc/optimiser.scala
@@ -5,6 +5,19 @@ import scala.collection.mutable.ListBuffer
 object optimiser {
 
   def optimise(prog: ListBuffer[Instruction]): ListBuffer[Instruction] = {
-    prog
+    removePushPop(prog)
+  }
+
+  private def removePushPop(prog: ListBuffer[Instruction]): ListBuffer[Instruction] = {
+    val newProg = ListBuffer[Instruction]()
+    var i = 0
+    while (i < prog.length) {
+      prog(i) match {
+        case Push(op) if prog(i+1) == Pop(op.asInstanceOf[Dest]) => i += 2
+        case _ => newProg += prog(i)
+          i += 1
+      }
+    }
+    newProg
   }
 }
