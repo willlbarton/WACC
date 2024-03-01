43869d4849a2f9f333f5c6576ceae958cc54a2b3
Francois Poh <fp522@ic.ac.uk>
2024-03-01 12:37:52 +0000

    [fp522] fix(s): use iterableOnce in lb

--- src/main/wacc/builtInFunctions.scala
+++ src/main/wacc/builtInFunctions.scala
@@ -476,10 +476,8 @@ object builtInFunctions {
 
     for (instruction <- instructions) {
       instruction match {
-        case inst: Instruction =>
-          resultBuffer += inst
-        case instIter: IterableOnce[_] if instIter.iterator.forall(_.isInstanceOf[Instruction]) =>
-          resultBuffer ++= instIter.asInstanceOf[IterableOnce[Instruction]]
+        case inst: Instruction => resultBuffer += inst
+        case instIter: IterableOnce[Instruction] => resultBuffer ++= instIter
         case _ =>
           throw new IllegalArgumentException(s"Unsupported type: ${instruction.getClass}")
       }
