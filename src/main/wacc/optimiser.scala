98d8750cc176e9980706564ade6975d9e4c60b3f
Francois Poh <fp522@ic.ac.uk>
2024-03-01 15:23:56 +0000

    [fp522] feat(optimiser): Add |> operator for peephole

--- src/main/wacc/optimiser.scala
+++ src/main/wacc/optimiser.scala
@@ -1,16 +1,50 @@
 package src.main.wacc
 
-import src.main.wacc.generator.lb
+import src.main.wacc.builtInFunctions.lb
 
 import scala.collection.mutable.ListBuffer
+import scala.language.implicitConversions
 
 object optimiser {
 
   def optimise(prog: ListBuffer[Instruction]): ListBuffer[Instruction] = {
-    peepN(peepN(prog, 2, removePushPop), 2, pushPopToMov)
+    val program =
+      AsmProgram(prog) |>
+        removePushPop |>
+        pushPopToMov
+    program.instrs
   }
 
-  private def peepN(prog: ListBuffer[Instruction], n: Int, f: ListBuffer[Instruction] => (ListBuffer[Instruction], Int)): ListBuffer[Instruction] = {
+  private def removePushPop(prog: ListBuffer[Instruction]): (ListBuffer[Instruction], Int) = {
+    prog.head match {
+      case Push(op) if prog(1) == Pop(op.asInstanceOf[Dest]) => lb() -> 2
+      case _ => lb(prog.head) -> 1
+    }
+  }
+
+  private def pushPopToMov(prog: ListBuffer[Instruction]): (ListBuffer[Instruction], Int) = {
+    prog.head match {
+      case Push(op) if prog(1) == Pop(op.asInstanceOf[Dest]) =>
+        lb(Mov(op, prog(1).asInstanceOf[Pop].dest)) -> 2
+      case _ => lb(prog.head) -> 1
+    }
+  }
+}
+
+private case class AsmProgram(instrs: ListBuffer[Instruction]) {
+
+  def apply(i: Int): Instruction = instrs(i)
+  def length: Int = instrs.length
+
+  def |>(f: ListBuffer[Instruction] => (ListBuffer[Instruction], Int)): AsmProgram = {
+    peepN(instrs, 2, f)
+  }
+
+  private def peepN(
+    prog: ListBuffer[Instruction],
+    n: Int,
+    f: ListBuffer[Instruction] => (ListBuffer[Instruction], Int)
+  ): AsmProgram = {
     val newProg = ListBuffer[Instruction]()
     var i = 0
     while (i < prog.length) {
@@ -18,21 +52,6 @@ object optimiser {
       newProg ++= inst
       i += step
     }
-    newProg
-  }
-
-  private def removePushPop(prog: ListBuffer[Instruction]): (ListBuffer[Instruction], Int) = {
-    prog.head match {
-      case Push(op) if prog.last == Pop(op.asInstanceOf[Dest]) => lb() -> 2
-      case _ => lb(prog.head) -> 1
-    }
-  }
-
-  private def pushPopToMov(prog: ListBuffer[Instruction]): (ListBuffer[Instruction], Int) = {
-    prog.head match {
-      case Push(op) if prog.last == Pop(op.asInstanceOf[Dest]) =>
-        lb(Mov(op, prog.last.asInstanceOf[Pop].dest)) -> 2
-      case _ => lb(prog.head) -> 1
-    }
+    AsmProgram(newProg)
   }
 }
